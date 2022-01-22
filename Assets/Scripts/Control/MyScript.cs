a309a8965bd0584b095dc14083e52c564f661ea9
LeandroNeto <leandrohock@gmail.com>
2022-01-21 22:33:08 -0300

    First version

---
+++ Assets/Scripts/Control/MyScript.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MyScript : MonoBehaviour {
+
+    protected Joysticks joystick;
+    protected JoyButton joybutton;
+    protected Rigidbody playerRigidbody;
+
+    // Use this for initialization
+    void Start () {
+        joystick = FindObjectOfType<Joysticks>();
+        joybutton = FindObjectOfType<JoyButton>();
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        playerRigidbody = GetComponent<Rigidbody>();
+        playerRigidbody.velocity = new Vector3(joystick.Horizontal * 100f,
+                                        playerRigidbody.velocity.y,
+                                        joystick.Vertical * 100f);
+	}
+}
